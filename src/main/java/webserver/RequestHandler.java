e9c0a065ff327ad0bf870c8b9da97bbc89a325a4
YuyoungRhee <bbclara@naver.com>
2025-04-16 02:36:12 +0900

    3.4.3.1 요구사항 1 - index.html 응답하기 (2,3단계)
    - Http 요청 정보의 첫 번째 라인에서 요청 URL을 추출한다.
    요청 url에 해당하는 파일을 webapp 디렉토리에서 읽어 전달한다.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,18 +2,20 @@ package webserver;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
-    private Socket connection;
+    private final Socket connection;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -28,17 +30,18 @@ public class RequestHandler extends Thread {
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
             DataOutputStream dos = new DataOutputStream(out);
 
-            StringBuilder sb = new StringBuilder();
-            while(true) {
-                String line = bufferedReader.readLine();
-                if(line == null || line.isEmpty()) {
-                    break;
-                }
-                sb.append("<pre>\n");
-                sb.append(line).append("\n");
-                sb.append("</pre>");
+            String line = bufferedReader.readLine();
+
+            if(line == null || line.isEmpty()) {
+                send400Error(dos);
+                return;
             }
-            byte[] body = sb.toString().getBytes();
+
+            String[] tokens = line.split(" ");
+            String url = tokens[1];
+
+            log.info("url: " + url);
+            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
 
             response200Header(dos, body.length);
             responseBody(dos, body);
@@ -65,5 +68,15 @@ public class RequestHandler extends Thread {
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private void send400Error(DataOutputStream dos) throws IOException {
+        String body = "400 Bad Request";
+        dos.writeBytes("HTTP/1.1 400 Bad Request\r\n");
+        dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+        dos.writeBytes("Content-Length: " + body.getBytes().length + "\r\n");
+        dos.writeBytes("\r\n");
+        dos.writeBytes(body);
+        dos.flush();
     }
 }
