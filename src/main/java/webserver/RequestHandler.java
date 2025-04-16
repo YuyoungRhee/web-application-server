bf26ad3fe672d5ad8e8380da85949fd8bc815fd6
YuyoungRhee <bbclara@naver.com>
2025-04-16 23:43:55 +0900

    3.4.3.2 요구사항 2 - GET 방식으로 회원가입하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,5 +1,6 @@
 package webserver;
 
+import db.DataBase;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.File;
@@ -9,8 +10,11 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Map;
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -42,13 +46,56 @@ public class RequestHandler extends Thread {
 
             log.info("url: " + url);
             File file = new File("./webapp" + url);
-            byte[] body = Files.readAllBytes(file.toPath());
 
-            String contentType = guessContentType(file.getName());
-            response200Header(dos, body.length, contentType);
-            responseBody(dos, body);
+            if(file.exists()) {
+                serveStaticFile(file, dos);
+                return;
+            }
+            handleUserCreate(url, dos);
+
         } catch (IOException e) {
             log.error(e.getMessage());
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private void serveStaticFile(File file, DataOutputStream dos) throws IOException {
+        byte[] body = Files.readAllBytes(file.toPath());
+
+        String contentType = guessContentType(file.getName());
+        response200Header(dos, body.length, contentType);
+        responseBody(dos, body);
+    }
+
+    private void handleUserCreate(String url, DataOutputStream dos) {
+        String queryString = url.split("\\?")[1];
+        Map<String, String> paramDatas = HttpRequestUtils.parseQueryString(queryString);
+
+        String userId = paramDatas.get("userId");
+        String password = paramDatas.get("password");
+        String name = paramDatas.get("name");
+        String email = paramDatas.get("email");
+
+        User user = new User(userId, password, name, email);
+        DataBase.addUser(user);
+
+        response201Header(dos);
+//        responseBody(dos, body);
+    }
+
+    private void response201Header(DataOutputStream dos) {
+        int lengthOfBodyContent = 0;
+
+        try {
+            dos.writeBytes("HTTP/1.1 201 Created \r\n");
+//            dos.writeBytes("Location: ");
+//            dos.writeBytes("Content-Type: application/json \r\n");
+//            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+            dos.flush();
+        } catch (IOException e) {
+            log.error(e.getMessage());
+            throw new IllegalStateException(e);
         }
     }
 
@@ -60,6 +107,7 @@ public class RequestHandler extends Thread {
             dos.writeBytes("\r\n");
         } catch (IOException e) {
             log.error(e.getMessage());
+            throw new IllegalStateException(e);
         }
     }
 
@@ -89,16 +137,21 @@ public class RequestHandler extends Thread {
             dos.flush();
         } catch (IOException e) {
             log.error(e.getMessage());
+            throw new IllegalStateException(e);
         }
     }
 
     private void send400Error(DataOutputStream dos) throws IOException {
         String body = "400 Bad Request";
-        dos.writeBytes("HTTP/1.1 400 Bad Request\r\n");
-        dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-        dos.writeBytes("Content-Length: " + body.getBytes().length + "\r\n");
-        dos.writeBytes("\r\n");
-        dos.writeBytes(body);
-        dos.flush();
+        try {
+            dos.writeBytes("HTTP/1.1 400 Bad Request\r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + body.getBytes().length + "\r\n");
+            dos.writeBytes("\r\n");
+            dos.writeBytes(body);
+            dos.flush();
+        } catch (IOException e) {
+            log.warn("Broken pipe or other IO error while sending 400: {}", e.getMessage());
+        }
     }
 }
